4281212b6714cc66cc862de85c33093a1e173025
kevin <kevin.ferin.3@gmail.com>
2020-01-23 16:55:27 +0100

    copy kevin 2

--- Webapp/copyForKevin.py
+++ Webapp/copyForKevin.py
@@ -123,9 +123,9 @@ elif navigation=='Question 2':
     df_q2 = query2(source).copy()
     st.dataframe(df_q2)
     #df = px.data.gapminder()
-    df = df_q2.groupby(["ActionGeo_CountryCode","Month"]).count()
-    fig = px.choropleth(df, locations="ActionGeo_CountryCode", color="numMentions", animation_frame="Month", range_color=[20,80], width=800, height=800)
-    st.plotly_chart(fig)
+    # df = df_q2.groupby(["ActionGeo_CountryCode","Month"]).count()
+    # fig = px.choropleth(df, locations="ActionGeo_CountryCode", color="numMentions", animation_frame="Month", range_color=[20,80], width=800, height=800)
+    # st.plotly_chart(fig)
     print("")
 
 
