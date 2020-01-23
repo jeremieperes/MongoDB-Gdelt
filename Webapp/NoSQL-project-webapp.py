7aff84191c704cb253a34a54b6ae6f0e374f2d04
Jérémie Peres <jeremieperes@gmail.com>
2020-01-23 21:54:05 +0100

    2154

--- Webapp/NoSQL-project-webapp.py
+++ Webapp/NoSQL-project-webapp.py
@@ -40,6 +40,13 @@ def read_mongo(collection, query={}, no_id=True):
 
     return df
 
+def iso(country):
+    pays = pycountry.countries.get(alpha_2=country.upper())
+    if pays is not None:
+        return pays.alpha_3
+    else:
+        return ''
+
 #########################################################################
 ###########################    Visualization    #########################
 #########################################################################
@@ -93,37 +100,29 @@ elif navigation=='Question 1':
 
     df_q1 = query1(year=year1, month=month1, day=day1, country=country1, language=language1)
 
-    df_q1['Couverture médiatique'] = df_q1.numArticles / df_q1.numEvent
-
-    def iso(country):
-        pays = pycountry.countries.get(alpha_2=country.upper())
-        if pays is not None:
-            return pays.alpha_3
-        else:
-            return ''
-
     df_q1['iso']=df_q1['pays'].apply(iso)
-
-    st.dataframe(df_q1, height=500)
-
-    fig = px.scatter(df_q1, x="numArticles", y="numEvent")
-    st.plotly_chart(fig)
 
     df_q1_agg = df_q1.groupby("iso").sum().reset_index()
 
-    fig = px.bar(df_q1_agg, x="iso", y="Couverture médiatique")
-    st.plotly_chart(fig)
+    df_q1_agg['Couverture médiatique'] = df_q1_agg.numArticles / df_q1_agg.numEvent
 
-    fig = px.choropleth(df_q1_agg, locations="iso", color="Couverture médiatique", range_color=[0,350], color_continuous_scale="RdYlGn")
-    st.plotly_chart(fig)
+    st.dataframe(df_q1, height=500)
 
-    # Ajouter histogrammes top 10 des pays et top 10 des langues
+    if country1 != "":
+        st.markdown("Nombre d'articles selon le nombre d'événement")
+        fig = px.scatter(df_q1, x="numArticles", y="numEvent")
+        st.plotly_chart(fig)
+
+    if country1 == "":
+        fig = px.choropleth(df_q1_agg, locations="iso", color="Couverture médiatique", range_color=[0,50], color_continuous_scale="RdYlGn")
+        st.plotly_chart(fig)
+
+        st.markdown("**Top 10 pays:**")
+        fig = px.bar(df_q1[df_q1[]], x="pays", y="numArticles")
+        st.plotly_chart(fig)
+
     st.markdown("**Top 10 langues:**")
     fig = px.bar(x=df_q1.langue, y=df_q1.numArticles)
-    st.plotly_chart(fig)
-
-    st.markdown("**Top 10 pays:**")
-    fig = px.bar(x=df_q1.pays, y=df_q1.numArticles)
     st.plotly_chart(fig)
 
 
