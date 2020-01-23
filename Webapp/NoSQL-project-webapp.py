4a4e4b7e7908e7c8fbaa007056bfb3308dfa3c4a
Jérémie Peres <jeremieperes@gmail.com>
2020-01-23 18:05:33 +0100

    df

--- Webapp/NoSQL-project-webapp.py
+++ Webapp/NoSQL-project-webapp.py
@@ -143,9 +143,9 @@ elif navigation=='Question 3':
     st.write(tone_theme)
 
     country = tone_country.set_index('Country').join(df_countries.Country.value_counts())
-    country.reset_index(inplace=True).rename(columns={'Country':'Number of articles'}, inplace=True)
+    country.reset_index(inplace=True)
 
-    fig = px.scatter(country, x="Tone", y="Number of articles")
+    fig = px.scatter(country, x="Tone", y="Country")
     st.plotly_chart(fig)
 
 
