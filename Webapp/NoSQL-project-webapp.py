b80e18dc4f936faccbf0505bdb6f01e0782abc6c
Jérémie Peres <jeremieperes@gmail.com>
2020-01-23 18:09:52 +0100

    fds

--- Webapp/NoSQL-project-webapp.py
+++ Webapp/NoSQL-project-webapp.py
@@ -146,7 +146,7 @@ elif navigation=='Question 3':
     country = country.rename(columns={'Country':'Number of articles'})
     country.reset_index(inplace=True)
 
-    fig = px.scatter(country, x="Tone", y="Number of articles")
+    fig = px.scatter(country, x="Tone", y="Number of articles", color='index')
     st.plotly_chart(fig)
 
 
