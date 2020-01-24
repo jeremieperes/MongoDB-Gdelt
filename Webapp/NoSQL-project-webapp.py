66ac1f9ed1c0a7e24d1344e7d36f2b1af5ab3f4c
Jérémie Peres <jeremieperes@gmail.com>
2020-01-24 03:25:48 +0100

    sf

--- Webapp/NoSQL-project-webapp.py
+++ Webapp/NoSQL-project-webapp.py
@@ -40,7 +40,7 @@ def read_mongo(collection, query={}, no_id=True):
 
     return df
 
-@cache(persist=True)
+@st.cache(persist=True)
 def iso(country):
     pays = pycountry.countries.get(alpha_2=country.upper())
     if pays is not None:
